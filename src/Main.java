324881eb5c60c9536245aef81cda6e5afd80837c
Nikola <nikola.nocon@gmail.com>
2020-02-22 14:14:38 +0100

    Added descriptive BMI

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your weight in kg");
+        System.out.println("What is your weight in kg?");
         float weight = sc.nextFloat();
-        System.out.println("What is your height in m");
+        System.out.println("What is your height in m?");
         float height = sc.nextFloat();
-        float bmi = bmi(weight, height);
-        System.out.println("Your bmi is " + bmi + "that quallifies as " + bmiDescriptive(bmi));
+        float bmi=bmi(weight, height);
+        System.out.println("Your bmi is "+bmi+"that quallifies as "+bmiDescriptive(bmi));
+
     }
 
-    private static bmiDescriptive( float bmi);{
-        if (bmi < 18.5) return "Underweight";
-        else if (bmi < 25) return "Normal";
+    private static String bmiDescriptive(float bmi) {
+        if(bmi<18.5) return "Underweight";
+        else if(bmi<25) return "Normal (healthy weight)";
         else return "Overweight";
     }
-        private static float bmi ( float weight, float height){
-        return weight / (height * height);
+
+    private static float bmi(float weight, float height) {
+        return weight/(height*height);
     }
-    }
+}
